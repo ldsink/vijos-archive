6149d67e40102f93eedf6b7b158db119a6d876a1
ZHOU Cheng <c.zhou@live.com>
2019-05-18 18:10:50 +0800

    use math/big

--- work/P1005 超长数字串.go
+++ work/P1005 超长数字串.go
@@ -2,33 +2,17 @@ package main
 
 import (
 	"fmt"
-	"strconv"
+	"math/big"
 	"strings"
 )
 
-func endlessEnumerate(a string) int {
-	var s string
-	var result int
-	var bit uint = 15
-	for i, j := 1, 1<<bit; i <= j; i++ {
-		s += strconv.Itoa(i)
-	}
-	result = strings.Index(s, a) + 1
-	length := len(s)
-	offset := len(a)
-	s = s[length-offset:]
-	for ; result == 0; {
-		for i, j := (1<<bit)+1, 1<<(bit+1); i <= j; i++ {
-			s += strconv.Itoa(i)
-		}
-		result = strings.Index(s, a) + 1
-		if result > 0 {
-			result = length - offset + result
-		}
-		length += len(s)
-		s = s[len(s)-offset:]
-	}
-	return result
+var zero = new(big.Int).SetInt64(0)
+var one = new(big.Int).SetInt64(1)
+var ten = new(big.Int).SetInt64(10)
+
+func getBigInt(s string) *big.Int {
+	i, _ := new(big.Int).SetString(s, 10)
+	return i
 }
 
 func max(a, b int) int {
@@ -37,6 +21,7 @@ func max(a, b int) int {
 	}
 	return b
 }
+
 func min(a, b int) int {
 	if a < b {
 		return a
@@ -44,98 +29,90 @@ func min(a, b int) int {
 	return b
 }
 
-func validateBitAndOffset(bit, offset int, a string) int64 {
+func validateBitAndOffset(bit, offset int, a string) *big.Int {
 	// 长度为 bit，在 a 中偏移量为 offset 的 num 是否符合条件，满足条件返回 num。否则返回 -1
 	numStr := a[offset : offset+bit]
 	if strings.HasPrefix(numStr, "0") {
-		return -1
+		return zero
 	}
 
-	num, _ := strconv.ParseInt(numStr, 10, 64)
-	var n int64
+	num := getBigInt(numStr)
+	n := new(big.Int)
 	var left, right int
 	// 检测左边是否满足条件，最多只需要检测一次
-	n = num - 1
+	n.Sub(num, one)
 	right = offset
 	if right > 0 {
-		if n < 1 {
-			return -1
+		if n.Cmp(one) < 0 {
+			return zero
 		}
-		nStr := strconv.FormatInt(n, 10)
+		nStr := n.String()
 		left = max(0, offset-len(nStr))
 		if a[left:right] != nStr[len(nStr)-(right-left):] {
-			return -1
+			return zero
 		}
 	}
 	// 检测右边是否满足条件
-	n = num + 1
+	n.Add(num, one)
 	left = offset + bit
 	length := len(a)
 	for left < length {
-		nStr := strconv.FormatInt(n, 10)
+		nStr := n.String()
 		right = min(length, left+len(nStr))
 		if a[left:right] != nStr[0:right-left] {
-			return -1
+			return zero
 		}
 		left += len(nStr)
-		n++
+		n.Add(n, one)
 	}
-
 	return num
 }
 
-func getBitNumOffset(bit int) int64 {
-	if bit < 1 {
-		return 0
-	}
-	if bit == 1 {
-		return 9
-	}
-	var n int64 = 1
-	for i := 1; i < bit; i++ {
-		n *= 10
-	}
-	n = n * 9
-	return n * int64(bit)
-}
-
-func getNumberPosition(num int64, offset int) int64 {
+func getNumberPosition(num *big.Int, offset int) *big.Int {
 	// 获得这个数字前面计算 offset 之后出现的位置
-	result := int64(offset)
-	if num < 10 {
-		result += num
+	result := big.NewInt(0 - int64(offset))
+	if num.Cmp(ten) < 0 {
+		result.Add(result, num)
 		return result
 	}
 
-	numStr := strconv.FormatInt(num, 10)
+	numStr := num.String()
 	length := len(numStr)
-	var start int64 = 1
+	start := one
 	for i := 1; i < length; i++ {
-		result += getBitNumOffset(i)
-		start *= 10
+		count := big.NewInt(9)
+		count.Mul(count, start)
+		count.Mul(count, big.NewInt(int64(i)))
+		result.Add(result, count)
+		start.Mul(start, ten)
 	}
-	result += int64(length)*(num-start) + 1
+
+	start.Sub(num, start)
+	curr := big.NewInt(int64(length))
+	curr.Mul(curr, start)
+	curr.Add(curr, big.NewInt(1))
+	result.Add(result, curr)
 	return result
 }
 
 func main() {
 	var a string
 	_, _ = fmt.Scanf("%s", &a)
-	n, _ := strconv.ParseInt(a, 10, 64)
-	var result int64
-	if n == 0 {
-		num, _ := strconv.ParseInt("1"+a, 10, 64)
-		result = getNumberPosition(num, -1)
-	} else {
-		for i, j := 1, len(a); i <= j && result == 0; i++ {
-			for offset := 0; offset < i && result == 0; offset++ {
-				num := validateBitAndOffset(i, offset, a)
-				if num > 0 {
-					result = getNumberPosition(num, offset)
-				}
+	//fmt.Println(endlessEnumerate(a)) // debug
+
+	n := getBigInt(a)
+	if n.Cmp(zero) == 0 {
+		num := getBigInt("1" + a)
+		fmt.Println(getNumberPosition(num, -1))
+		return
+	}
+	for i, j := 1, len(a); i <= j; i++ {
+		for offset := 0; offset < i; offset++ {
+			num := validateBitAndOffset(i, offset, a)
+			if num.Cmp(zero) > 0 {
+				fmt.Println(getNumberPosition(num, offset))
+				return
 			}
 		}
 	}
-	fmt.Println(result)
-	//fmt.Println(endlessEnumerate(a)) // debug
 }
